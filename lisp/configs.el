334c0410f649b0fd3ded70118ce9f77a09a4b18a
ct2cx <ct2cx@protonmail.com>
2025-12-05 13:59:55 +0600

    configs: change font and delete wrap line indicators

--- lisp/configs.el
+++ lisp/configs.el
@@ -3,7 +3,9 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(set-face-attribute 'default nil :family "IosevkaTerm Nerd Font" :height 150)
+(set-face-attribute 'default nil :family "JetBrains Mono Nerd Font" :height 140)
+; deletes the left and right wrap line indicator
+(setf (cdr (assq 'continuation fringe-indicator-alist)) '(nil nil))
 
 (defun reload-init-file()
   ; makes the function callable interactively using M-x reload-init-file.
